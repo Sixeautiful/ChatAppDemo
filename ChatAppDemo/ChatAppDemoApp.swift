7e714a2217c0a2c4e8900863481c4ead7267e938
Sixeautiful <vladerastatkevich2015@gmail.com>
2022-05-31 13:06:20 +0200

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Kirill Willer on 31.05.22.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
